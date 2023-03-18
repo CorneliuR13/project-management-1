6634d04c43ecaaa0ccaae9f28f8744c5c3154109
Alexandru Haruta <alexandruharuta.18@gmail.com>
2023-03-18 22:42:38 +0200

    DbContext

--- ProjectManagementApp.BusinessLogic/DBModel/UserContext.cs
+++ ProjectManagementApp.BusinessLogic/DBModel/UserContext.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ProjectManagementApp.BusinessLogic.DBModel
 {
-    internal class UserContext
+    class UserContext : DbContext
     {
+        public UserContext() :
+                base("name=ProjectManagementApp")
+            { }
+        public virtual DbSet<UDbTable> Users { get; set; }
     }
 }
