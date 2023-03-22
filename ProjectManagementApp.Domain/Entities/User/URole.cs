614fa735a8c8e0faf2bb8e12b8de14c56009e3f3
Cornel Razborschi <razborschicorneliu@gmail.com>
2023-03-22 12:38:43 +0200

    User Roles

--- ProjectManagementApp.Domain/Entities/User/URole.cs
+++ ProjectManagementApp.Domain/Entities/User/URole.cs
@@ -2,5 +2,16 @@
 {
     public class URole
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role UserRole { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        Manager,
+        Employee,
+        Guest
     }
 }
